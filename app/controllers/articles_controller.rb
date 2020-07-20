915cdaab65380da67767678c5a9e9bbde8f3829d
Fraciscv <Francisco.Andaur@icloud.com>
2020-07-20 09:08:19 -0400

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,5 +19,23 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
-  # add edit and update methods here
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    #Store the query in an instance variable.
+    @article = Article.find(params[:id])
+    #Update the values passed from the form 
+    @article.update(article_params)
+    #Redirect the user to the show page so they can see the updated record.
+    redirect_to article_path(@article)
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+
 end
