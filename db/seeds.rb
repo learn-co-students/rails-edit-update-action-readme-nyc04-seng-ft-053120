320a628219dada588704cb3e5148fec0329ceecf
Soxinmypocket <shawnaejohnson6@gmail.com>
2020-07-20 22:04:55 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Article.create([
+    {
+        title: "New York Daily Weather",
+        description: "New York newspaper"
+    }
+])
+
+puts "done seeding"
